266b5efbd24d588f69ae87c5bce8b56c378ec160
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2017-09-19 09:34:48 +0200

    Added sudocmdgroup toe hiera and cleaned up profile

--- puppet/modules/profile/manifests/ipa/server.pp
+++ puppet/modules/profile/manifests/ipa/server.pp
@@ -1,20 +1,6 @@
 class profile::ipa::server {
   include ipa::server
 
-#  ipa_dnsrecord { 'test':
-#    ensure         => present,
-#    zone           => 'cmc.lan',
-#    ipaddress      => '10.10.10.117',
-#    require        => Ipa_zonerecord['10.in-addr.arpa', 'cmc.lan']
-#  }
-#  ipa_zonerecord { '10.10.10.in-addr.arpa':
-#    ensure         => present,
-#    ttl            => '1800',
-#  }
-#  ipa_zonerecord { 'cmc.lan':
-#    ensure         => present,
-#    ttl            => '3600'
-#  }
   ipa_forwardzonerecord { 'windows.cmc.lan':
     ensure         => absent,
     forwarder      =>   '10.10.10.116'
@@ -33,14 +19,6 @@ class profile::ipa::server {
     ensure    => absent,
     groupname => 'testhostgroup',
     type      => 'group'
-  }
-  ipa_sudorule { 'test':
-    description => 'Test rule'
-  }
-
-  ipa_sudoruleoption { 'test_authenticate':
-    option   => '!authenticate',
-    sudorule => 'test'
   }
 
   $httpports = ['80','443']
